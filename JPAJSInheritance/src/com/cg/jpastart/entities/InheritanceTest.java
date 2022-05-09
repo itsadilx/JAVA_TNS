08c3796edc95267613142ddf013dbb82f7e80dc0
Adil Shaikh <adilshaikhhh@outlook.com>
2022-05-09 19:44:01 +0530

    JPA Inheritance

---
+++ JPAJSInheritance/src/com/cg/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,36 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("joyce");
+		employee.setSalary(5000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("suma");
+		manager.setSalary(800000);
+		manager.setDepartmentName("sales");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
