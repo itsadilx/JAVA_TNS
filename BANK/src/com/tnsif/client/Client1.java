5191d506df5c61bea704eef27e721bd4fc486cb5
Adil Shaikh <adilshaikhhh@outlook.com>
2022-05-09 14:06:32 +0530

    JPA Crud Operations

--- BANK/src/com/tnsif/client/Client1.java
+++ BANK/src/com/tnsif/client/Client1.java
@@ -10,8 +10,8 @@ public class Client1 {
 	public static void main(String[] args) 
 	{
 		MMBankFactory mmbankfactory=new MMBankFactory();
-		mmbankfactory.getNewSavingAccount(111, "Adil", 1000, true);
-		mmbankfactory.getNewCurrentAccount(123, "Rahul", 3456, 50);
+		mmbankfactory.getNewSavingAccount(1234567,"Adil", 1000, true);
+		mmbankfactory.getNewCurrentAccount(9876543,"Rahul", 3456, 50);
         
         MMSavingAcc mmsaving=new MMSavingAcc(111, "Adil", 1000, true);
         mmsaving.getaccNo();
